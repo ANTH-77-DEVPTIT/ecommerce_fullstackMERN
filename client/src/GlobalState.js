fa1dbd1a7521f13b44f570076faddfdd51929173
Trương Hùng An <truonghungan2000@gmail.com>
2022-06-26 20:43:21 +0700

    FE: handle refreshtoken

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from "react";
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
 import ProductsApi from './api/productApi'
 const GlobalState = createContext();
 
 const DataProvider = ({ children }) => {
     const [token, setToken] = useState(false)
+
+    const refreshToken = async () => {
+        const res = await axios.get('/user/refresh_token')
+        setToken(res.data.accesstoken)
+    }
+
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin) refreshToken()
+    }, [])
 
     const state = {
         token: [token, setToken],
